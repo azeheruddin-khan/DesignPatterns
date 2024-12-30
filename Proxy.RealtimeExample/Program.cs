f9002acd1a3c3ff6ce56aa695804ec6184633a89
azeheruddin khan <khan.azeheruddin@gmail.com>
2024-12-30 22:45:38 +0530

    Proxy Pattern  - Added realtime example

---
+++ Proxy.RealtimeExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proxy.RealtimeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
