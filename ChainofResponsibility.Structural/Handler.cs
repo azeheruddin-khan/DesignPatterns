ef0c3afd0739db7ba611cfbe87eabcc6221ed154
azeheruddin khan <khan.azeheruddin@gmail.com>
2025-01-02 22:57:38 +0530

    ChainofResponsibility - Structural

---
+++ ChainofResponsibility.Structural/Handler.cs
@@ -0,0 +1,14 @@
+﻿namespace ChainofResponsibility.Structural
+{
+    public abstract class Handler
+    {
+        protected Handler successor;
+        public void SetSuccessor(Handler handler)
+        {
+            this.successor = handler;
+        }
+
+        public abstract void HandleRequest(int request);
+
+    }
+}
---
+++ ChainofResponsibility.Structural/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChainofResponsibility.Structural
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Handler handleZeroToTen = new ConcreteHandler1();
+            Handler handleElevenToTwenty = new ConcreteHandler2();
+            Handler handleTwentyOneToThirtly = new ConcreteHandler3();
+            
+
+            handleZeroToTen.SetSuccessor(handleElevenToTwenty);
+            handleElevenToTwenty.SetSuccessor(handleTwentyOneToThirtly);
+
+            int[] numbers = { 10, 2, 3, 30, 21, 25, 15 };
+
+            foreach (int number in numbers)
+            {
+                handleZeroToTen.HandleRequest(number);
+            }
+        }
+    }
+}
